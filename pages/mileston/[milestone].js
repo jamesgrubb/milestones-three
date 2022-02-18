13fc9c87873ba1f8a54c9a5f3979899435569e1b
jamesgrubb <22652+jamesgrubb@users.noreply.github.com>
2022-02-18 16:57:50 +0000

    changes

--- pages/mileston/[milestone].js
+++ pages/mileston/[milestone].js
@@ -1,26 +1,57 @@
 import { v2 as cloudinary } from 'cloudinary';
 
 export const getServerSideProps = async ({ params }) => {
+	console.log(
+		'🚀 ~ file: [milestone].js ~ line 4 ~ getServerSideProps ~ params',
+		params
+	);
+	const { milestone } = params;
 	cloudinary.config({
 		cloud_name: process.env.CLOUDINARY_NAME,
 		api_key: process.env.CLOUDINARY_API,
 		api_secret: process.env.CLOUDINARY_SECRET,
 		secure: true,
 	});
-	const milestone = cloudinary.video('Milestones/Video/Dancing', {
+	const video = cloudinary.video('Milestones/Video/Dancing', {
 		autoplay: 'autoplay',
+		controls: true,
 		// width: 960,
 		// height: 540,
+		transformation: [
+			{ overlay: 'Milestones:bg' },
+			{
+				x: 0,
+				y: 0,
+				width: 300,
+				height: 300,
+				background: 'red',
+			},
+			{
+				color: 'black',
+				overlay: {
+					font_family: 'Bogam.otf',
+					font_size: 300,
+					text: milestone,
+				},
+			},
+			{
+				flags: ['layer_apply'],
+				x: 0,
+				y: 0,
+				width: 100,
+				height: 100,
+				crop: 'fill',
+			},
+		],
 	});
 	return {
 		props: {
-			milestone,
+			video,
 		},
 	};
 };
-
-const Milestone = ({ milestone }) => {
-	return <div dangerouslySetInnerHTML={{ __html: milestone }}></div>;
+const Milestone = ({ video }) => {
+	return <div dangerouslySetInnerHTML={{ __html: video }}></div>;
 };
 
 export default Milestone;
