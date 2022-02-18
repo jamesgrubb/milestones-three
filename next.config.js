5d4deb9e3d846493647f84732052f61c0afc9737
jamesgrubb <22652+jamesgrubb@users.noreply.github.com>
2022-02-18 15:27:25 +0000

    ddeleted tut files

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true,
+};
 
-module.exports = nextConfig
+module.exports = {
+	images: {
+		domains: ['vignette.wikia.nocookie.net'],
+	},
+};
+
+module.exports = nextConfig;
